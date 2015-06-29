6ce0729f6dc01fd38ac11500e996bc92568c17e5
Venise <Venise@users.noreply.github.com>
2015-06-29 21:35:01 +0800

    modify quickSort

--- src/com/sort/bubble/BubbleSort.java
+++ src/com/sort/bubble/BubbleSort.java
@@ -3,11 +3,11 @@ package com.sort.bubble;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] array = {1,9,3,6,33,88,2,5,7};
+		int[] array = {49,38,65,97,76,13,27,49};
 		array = bubble(array);
-		for(int i=0; i<array.length; i++)
-			System.out.print(array[i]+ " ");
-		System.out.println();
+//		for(int i=0; i<array.length; i++)
+//			System.out.print(array[i]+ " ");
+//		System.out.println();
 	}
 	
 	/*
@@ -16,7 +16,7 @@ public class BubbleSort {
 	 * 初始：inner=0,outer=array.length-1
 	 */
 	private static int[] bubble(int[] array){
-		int inner, outer;
+		int inner, outer;//两个指针，内层和外层
 		for(outer = array.length-1; outer>0; outer--){
 			for(inner=0; inner < outer; inner++) {
 				if(array[inner] > array[inner+1]){
@@ -25,6 +25,19 @@ public class BubbleSort {
 					array[inner+1] = temp;
 				}
 			}
+			
+			if(outer == array.length-1){
+				System.out.println("最大的数：" + array[array.length-1]+ " ");
+			}
+			if(outer == array.length-2){
+				System.out.println("倒数第二大的数：" + array[array.length-2]+ " ");
+			}
+			if(outer == array.length-3){
+				System.out.println("倒数第三大的数：" + array[array.length-3]+ " ");
+			}
+			for(int i=0; i<array.length; i++)
+				System.out.print(array[i]+ " ");
+			System.out.println();
 		}
 		return array;
 	}
--- src/com/sort/select/SelectSort.java
+++ src/com/sort/select/SelectSort.java
@@ -18,10 +18,10 @@ public class SelectSort {
 	private static int[] select(int[] array){
 		int outer, inner, min;
 		for(outer=0; outer<array.length-1; outer++){
-			min = outer;
+			min = outer;//初始化min指针的索引
 			for(inner=outer+1; inner< array.length; inner++){
 				if(array[inner] < array[min])
-					min = inner;
+					min = inner;//只查找最小值，不交换，一趟比较结束后，找到最小值，再进行交换。
 			}
 			int temp = array[outer];
 			array[outer] = array[min];
